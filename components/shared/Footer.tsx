c2d0310b6710ac18a47993100882938ba603b003
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2024-01-14 20:39:38 +0700

    Update footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col ga-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="logo"
+          />
+        </Link>
+        <p>{currentYear} Evently. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
